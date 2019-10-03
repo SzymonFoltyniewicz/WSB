bb27b028174a399b5ab2fc9c9a1b0f229b62b9d9
SzymonFoltyniewicz <szymon.foltyniewicz1@wp.pl>
2019-10-03 12:09:18 +0200

    1 Coś

---
+++ podstawy_progamowania/Nazwa 1/Nazwa 1/Nazwa 1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nazwa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             
+            Console.Write("WSB");
+            Console.WriteLine("Nowa linia");
+
+            Console.Write("Proszę podać swoje imię");
+
+            string name = Console.ReadLine();
+            //string to komenda pytania//
+            
+            
+            Console.Write("Masz na imię:");
+            Console.WriteLine(name);
+            //Typ danych to string//
+            int age = 20;
+            Console.WriteLine(age);
+            uint number = 10;
+            //uint może zostać zapisane z minusem
+            //typ float
+            float z = 12.55F;
+            //+konkatenacja 
+            Console.WriteLine("Zmienna Z wynosi" + z);
+
+            Console.WriteLine("Program obliczający pole prostokąta");
+
+            Console.WriteLine("Proszę podać długość");
+
+            string zmienna1 = Console.ReadLine();
+            int dl1 = int.Parse(zmienna1);
+            //parse zamienia wyraz na liczby//
+
+            Console.WriteLine("Proszę podac szerokość");
+            string zmienna2 = Console.ReadLine();
+            int szer = int.Parse(zmienna2);
+            //float x = float.Parse(Console.readline()); aby wyeliminować string//
+            int obwod = (2 * dl1 + 2 * szer);
+            Console.WriteLine("Obwód wynosi:" + obwod);
+            //  x*= bokB jest to równoznaczne z x= x * bokB
+
+
+            int x, y = 10, z;
+            // int zmienna = 10;//
+            int zmienna = new int()
+
+
+            
+
+        
+
+            Console.ReadKey();
+            
+        }
+    }
+}
