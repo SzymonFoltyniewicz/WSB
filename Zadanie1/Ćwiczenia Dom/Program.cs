05e7aa5513f27c2ef825ab2dd9b3b49d6379f441
SzymonFoltyniewicz <56110968+SzymonFoltyniewicz@users.noreply.github.com>
2019-10-05 21:57:41 +0200

    Add files via upload

---
+++ Zadanie1/Ćwiczenia Dom/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Zadanie_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Obliczający pole prostokąta");
+            //Nagłówek
+            Console.WriteLine("Proszę o podanie długości prostokąta");
+            int x = int.Parse(Console.ReadLine());
+            //Pierwsza zmienna
+            Console.WriteLine("Proszę o podanie szerokości prostokąta");
+
+            int y = int.Parse(Console.ReadLine());
+            //Druga zmienna
+            Console.WriteLine("Pole prostokąta wynosi:" + (x * y) + "cm/kw");
+            //Obliczenie końcowe
+            Console.ReadKey();
+        }
+    }
+}
