22d316930d04b7dd8b80c7adcd0a40a16dcd1751
SzymonFoltyniewicz <56110968+SzymonFoltyniewicz@users.noreply.github.com>
2019-10-17 12:13:03 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,148 +1,119 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Cwiczenia
+namespace Zajęcia
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Program obliczający pole prostokąta");
-            Console.WriteLine("Proszę liczbę długość ");
-            string x = Console.ReadLine();
-            int y, c;
-            Console.WriteLine("Prosze podać szerokość");
-            string z = Console.ReadLine();
-            //
-            if ((int.TryParse(x, out y)) && int.TryParse(z, out c) == true)
-            { Console.WriteLine("Pole prostokąta wynosi {0}", y * c); }
-            else Console.WriteLine("Podana wartość jest błędna");
-            Console.ReadKey();
+            Console.WriteLine("Hello World!");
+            //Napisz program ktory za pomocą instrukcji do while dla danych wartości zmieniających się w przedziale od 0 do 15 oblicza wartość funkcji y = 4x
 
-            Console.WriteLine("Kalkulator BMI");
-            Console.WriteLine("Prosze podac swoją wagę w kg");
-            string waga = Console.ReadLine();
-            float wg, wz;
-            Console.WriteLine("Prosze podać swój wzrost w metrach");
-            string wzrost = Console.ReadLine();
-            if (float.TryParse(waga, out wg) == true && (float.TryParse(wzrost, out wz) == true))
+            //int   x = 1;
+            //int suma= 0;
+
+            //do
+            //{
+            //    y = 4 * x;
+            //    Console.WriteLine("x ={0}\ty = {1}", x, y);
+            //        x++;
+            //} while (x <= 15);
+
+            //Napisz program znajdujący się powyżej za pomocą pętli while
+
+            //while(x<=15)
+            //{ y = x * 4;
+            //Console.WriteLine("x={0}\ty = {1}",x,y);
+            //    x++;
+            //}
+
+            //napisz program który za pomocą instrukcji while dla danych wartości x zmieniających się w przedziale od 1 do 50 obliczy ich sumę 
+
+            //while (x <= 50)
+            //{
+            //    suma += x;
+            //    x ++;
+            //}
+            //Console.WriteLine("Suma wynosi {0}", suma);
+            //napisz program który za pomocą pętli for sumuje liczby nieparzyste z przedziału od 1 do 10 
+            //int suma = 0;
+            //for (int x = 1; x <=10 ; x++)
+            //{
+            //    if (x % 2 == 0)
+
+            //        suma += x;
+
+            //}
+            //Console.WriteLine("Suma liczb parzystych {0}", suma);
+
+            //int  n = 10, row , cow;
+            //row = 1;
+
+            //do
+            //{
+            //    cow = 1;    
+
+            //    do
+            //    {
+
+            //        Console.Write
+
+
+            //       (row * cow + "\t");
+            //        cow++;
+
+
+            //    } while (cow <= n);
+            //    row++;
+            //    Console.WriteLine();
+
+            //} while (row  <= n);
+            //Napisz program który wywietli duże litery alfaabetu od a do z i od z do a z wykoszystaniem pętli for
+
+
+            //for (char x = 'A'; x <= 'Z'; x++)
+            //{
+            //    if (x < 'Z')
+            //        Console.Write(x + ",");
+            //    else
+            //        Console.Write(x + ".");
+            //}
+
+            //        int i = 1;
+            //        for (; i <= 10; i++)
+            //        {
+            //            if (i == 3 || i == 6 || i == 7)
+            //                break;
+            //            else
+            //                Console.Write("{0} ", i);
+
+            //        }
+
+
+            //Napisz program który w 10-cio elementowej tablicy jednowymiarowej umieszcza liczby całkowite od 1 do 10
+            int n = 10, i;
+            //deklaracja tablicy typpu całkowitego o rozmiarze n =10
+
+            int[] tab = new int[n];
+
+            for (i = 0; i < tab.Length; i++)
+            { tab[i] = i; }
+
+            for (i = 0; i < tab.Length; i++)
+                Console.WriteLine(tab[i]);
+            //Napis program w 5cio elementowej tablicy jednowymiarowej o nazwie colors umieszcza nazwy kolorów podane przez użytkownika i wyświetl kolory w formaci kolror 1, kolro2 
+
+            string[] colors = new string[5];
+            string color;
+            for (i = 0; i < colors.Length; i++)
             {
-                float BMI = wg / (wz * wz);
-                Console.WriteLine("Twoje BMI wynosi {0}", BMI);
-                Console.WriteLine("Oznacza to że");
-                if (BMI < 18.5)
-                { Console.WriteLine("Wygłodzenie"); }
-                else if (18.5 <= BMI && BMI < 24.99)
-                { Console.WriteLine("Waga prawiddłowa"); }
-                else Console.WriteLine("Nadwaga");
+                Console.Write("Podaj ulubiony kolor:");
+                color = Console.ReadLine();
+                colors[i] = color;
             }
-
-
-            Console.WriteLine("1 - Pole kwadratu\n2 - pola koła");
-            Console.Write("Wybierz wartość");
-            string x1;
-            x1 = Console.ReadLine();
-            switch (x1)
-            {
-                case "1":
-                    Console.Clear();
-                    Console.WriteLine("Pole kwadratu");
-                    Console.WriteLine("Prosze podać długość boku");
-                    string dl = Console.ReadLine();
-                    float dl1;
-                    if (float.TryParse(dl, out dl1) == true)
-                    { Console.WriteLine("Pole kwadratu wynosi {0}", dl1 * dl1); }
-                    else Console.WriteLine("Podałeś błędne dany");
-
-
-                    break;
-                case "2":
-                    Console.Clear();
-                    Console.WriteLine("Pole koła");
-                    Console.WriteLine("Prosze podać promień koła");
-                    string r1 = Console.ReadLine();
-                    double r;
-                    if (double.TryParse(r1, out r) == true)
-                    {
-                        double Pole = Math.PI * r * r;
-                        Console.WriteLine("Pole koła wynosi: {0:##.##}", Pole);
-                    }
-                    else Console.WriteLine("Podaeś błędne dany");
-
-                    break;
-                default:
-                    Console.WriteLine("Podaleś błędne dane");
-                    break;
-            }
-
-            Console.WriteLine("inicjalizacji , warunek,dekrementacja or inkrementacja");
-
-
-            for (int i = 1; i < +10; i++)
-            {
-                Console.WriteLine(i + " ");
-
-
-            }
-            Console.WriteLine("");
-            //Wyświatl liczb parzyste z przedziału od 5 do 20 w porzadku malejącym
-
-            for (int x = 20; x >= 5; x--)
-            {
-                if (x % 2 == 0)
-                    Console.WriteLine("Liczba {0} jest parzysta", x);
-
-            }
-            //wyswiatl choinkę na ekranie  wysokość podaje uzytkownik
-
-            char K = '*';
-            Console.WriteLine("Proszę podać wysokośc choinki");
-            int p = int.Parse(Console.ReadLine());
-
-            for (int i = 1; i <= p; i++)
-            {
-                Console.WriteLine("");
-                for (int j = 1;  j <= i; j++)
-                    Console.Write(K);
-            }
-            Console.ReadKey();
-
-            
-            
-
-
-
-
-
-
-
-
-
-
-
-
-
-       
-
-
-
-
-            
-
-
-
-           
-
-
-
-
-
-
-
-
+            Console.Clear();
+            Console.WriteLine("Ulubione kolory:");
+            int x = 0, count
 
 
 
@@ -150,5 +121,5 @@ namespace Cwiczenia
 
 
         }
-    }
+    }   
 }
